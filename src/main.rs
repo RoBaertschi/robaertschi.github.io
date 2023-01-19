badadc3477c05d33c58f9f8fa3c588fd2968dd3c
RoBaertschi <rtmbaertschi007@gmail.com>
2023-01-19 11:31:41 +0100

    finished for now. create book

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn app() -> Html {
             <nav class={classes!("navbar")} role="navigation"> 
                 <div class="navbar-brand">
                     <a class="navbar-item" href="index.html">
-                        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                        <img src="rb.png" width="28" height="28"/>
                     </a>
                 
                     <a {onclick} role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" id="burger">
@@ -44,19 +44,41 @@ fn app() -> Html {
                 </div>
 
             </nav>
-            <section>
-                <div class="container content">
+            <section class={classes!("is-fullheight")}>
+                <div class="container content is-fullheight">
                     <h1 class={classes!("title")}>{ "Welcome to RoBaertschi's Website" }</h1>
                     <p>{"Here you'll find some of my Projects. Here a list of my Projects that i am currently working on."}</p>
                     <ul>
                         <li>
-                            <a>
+                            <a href={"book.html"}>
                                 {"Minecraft Getting Started with Modding Book"}
                             </a>
                         </li>
+                        <li>
+                            <a href={"https://github.com/RoBaertschi/swisscheeseclient"}>
+                                {"Swisscheeseclient"}
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"https://github.com/RoBaertschi/stackable"}>
+                                {"Stackable"}
+                            </a>
+                        </li>
+                        <li>
+                            {"More soon"}
+                        </li>
+                        
                     </ul>
                 </div>
             </section>
+
+            <footer class={classes!("footer")}>
+                <div class={classes!("content", "has-text-centered")}> 
+                    {"Big Thanks to:"}
+                    <br/>
+                    {"Icon from "} <a href="https://www.flaticon.com/free-icons/rb" title="rb icons">{"Rb icons created by Muhammad_Usman - Flaticon"}</a>
+                </div>
+            </footer>
 
         </>    
     }
